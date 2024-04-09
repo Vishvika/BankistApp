7831879ed0f79cfc06ad2e5abf26d1e2726ca89a
Vishvika Shukla <95911010+Vishvika@users.noreply.github.com>
2024-04-09 18:06:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@
 /////////////////////////////////////////////////
 // Data
 const account1 = {
-  owner: 'Jonas Schmedtmann',
+  owner: 'Vishvika Shukla',
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
   interestRate: 1.2, // %
   pin: 1111,
@@ -21,7 +21,7 @@ const account2 = {
 };
 
 const account3 = {
-  owner: 'Steven Thomas Williams',
+  owner: 'Charles Sainz',
   movements: [200, -200, 340, -300, -20, 50, 400, -460],
   interestRate: 0.7,
   pin: 3333,
